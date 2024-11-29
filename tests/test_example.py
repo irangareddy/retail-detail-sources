9fd38fdf4f56b04187ea34190e0c8c7d65a69d90
Ranga Reddy <itsmerr17@gmail.com>
2024-11-29 14:20:33 -0800

    package setup

---
+++ tests/test_example.py
@@ -0,0 +1,11 @@
+"""Example tests.
+
+Remove this and other example files after bootstrapping your project.
+"""
+
+from retail_data_sources.example import double
+
+
+def test_example() -> None:
+    """Example Test"""
+    assert double(10) == 10 * 2
